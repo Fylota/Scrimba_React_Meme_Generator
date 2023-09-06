db6a0ba635d331779dfbaad26dcebe9a156f2d27
Ágota Fülöp <fulop.agota01@gmail.com>
2023-09-06 13:43:59 +0200

    made API call async

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -12,9 +12,12 @@ export default function Meme() {
     const [allMemes, setAllMemes] = useState([]);
 
     useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMemes(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
     }, [])
 
     function getMemeImg() {
